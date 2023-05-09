2bb6625eedbd1532e4b905b9ef332c22c23a2cfe
TecnoMayte <114059096+TecnoMayte@users.noreply.github.com>
2023-05-09 08:50:44 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,21 +10,12 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.AB, function () {
     if (password == ENTRY) {
         basic.showIcon(IconNames.Yes)
-        pins.servoWritePin(AnalogPin.P0, 90)
+        basic.pause(100)
         basic.clearScreen()
     } else {
         basic.showIcon(IconNames.No)
-        music.ringTone(262)
         basic.pause(500)
-        music.rest(music.beat(BeatFraction.Whole))
         basic.clearScreen()
-        if (password != ENTRY) {
-            contador += 1
-            if (contador == 3) {
-                music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Forever)
-                basic.showString("BLOQUEADO")
-            }
-        }
     }
     basic.pause(100)
 })
@@ -32,29 +23,9 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     ENTRY = "" + ENTRY + "B"
 })
-// recibe el mensaje de cuidado de la otra radio
-// 
-radio.onReceivedMessage(RadioMessage.message1, function () {
-    basic.showString("¡CUIDADO!")
-})
 // al iniciar establecemos las  variables y creamos una contraseña
 // 
-let contador = 0
 let ENTRY = ""
 let password = ""
-password = "BABBA"
+password = "ABBA"
 ENTRY = ""
-contador = 0
-pins.servoWritePin(AnalogPin.P0, 0)
-// establecemos la radio
-// 
-basic.forever(function () {
-    radio.setGroup(1)
-})
-// si la contraseña esta 3 veces mal se manda una señal de cuidado a la otra radio
-// 
-basic.forever(function () {
-    if (contador == 3) {
-        radio.sendMessage(RadioMessage.message1)
-    }
-})
