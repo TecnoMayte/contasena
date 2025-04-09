d2198dac0ef816872b691a9c4592a2d454c0bddd
TecnoMayte <114059096+TecnoMayte@users.noreply.github.com>
2025-04-09 09:26:47 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,30 +3,34 @@ enum RadioMessage {
 }
 // al pulsar el boton A, se detecta como digito de la contraseña.
 input.onButtonPressed(Button.A, function () {
-    ENTRY = "" + ENTRY + "A"
+    código = "" + código + "A"
 })
 // al presionar A+B, se introduce la contraseña, una vez marcada con A y B, si es correcta se muestra un tick y si es incorrecta una X, si es 3 veces incorrecta se bloquea y sueno un timbre de emergencia.
-// 
 input.onButtonPressed(Button.AB, function () {
-    if (password == ENTRY) {
+    if (password == código) {
         basic.showIcon(IconNames.Yes)
         basic.pause(100)
         basic.clearScreen()
-        ENTRY = ""
+        código = ""
     } else {
         basic.showIcon(IconNames.No)
         basic.pause(500)
         basic.clearScreen()
-        ENTRY = ""
+        código = ""
+        contador += 1
+        if (contador == 3) {
+            basic.showString("SOCORRO")
+        }
     }
 })
 // al presionar el boton B, es lo mismo que el boton A, se detecta como digito de la contraseña
 input.onButtonPressed(Button.B, function () {
-    ENTRY = "" + ENTRY + "B"
+    código = "" + código + "B"
 })
 // al iniciar establecemos las  variables y creamos una contraseña
-// 
-let ENTRY = ""
+let contador = 0
+let código = ""
 let password = ""
 password = "ABBA"
-ENTRY = ""
+código = ""
+contador = 0
